659a00bd625a4a667929c2734a6f05da7a9630bd
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-08-01 11:58:18 +0500

    feat: initialize base frontend layout with header, footer, and routing structure

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopFooter from './TopFooter'
+import CopyRight from './CopyRight'
+
+const Footer = () => {
+    return (
+        <footer>
+            <TopFooter />
+            <CopyRight />
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/Frontend/index.jsx
+++ src/pages/Frontend/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const Frontend = () => {
   return (
     <>
+    <Header />
     <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='*' element={<div>NotFound</div>}/>
     </Routes>
+    <Footer />
     </>
   )
 }
