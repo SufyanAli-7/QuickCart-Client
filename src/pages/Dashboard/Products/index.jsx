57d452dd99ee17fffef82bffd98239c67ee1ec39
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-08-01 20:25:51 +0500

    feat: add dashboard product management routing and placeholder components

--- src/pages/Dashboard/Products/index.jsx
+++ src/pages/Dashboard/Products/index.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import All from './All'
+import Add from './Add'
+import Edit from './Edit'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <Routes>
+      <Route path="/" element={<All />} />
+      <Route path="/add" element={<Add />} />
+      <Route path="/edit/:id" element={<Edit />} />
+    </Routes>
   )
 }
 
