6a6d23f2b8eaa7f725c5f28f70b037d9b1d8235e
Sufyan Ali <msufyan.202105869@gcuf.edu.pk>
2026-08-03 10:35:40 +0500

    feat: add ScrollToTop component and integrate it into the main application routes

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -6,16 +6,20 @@ import Auth from './Auth'
 import Page404 from '@/components/Misc/Page404'
 import { useAuth } from '@/context/AuthContext'
 import ProtectedRoute from '@/components/Misc/ProtectedRoute'
+import ScrollToTop from '@/components/Misc/ScrollToTop'
 
 const Index = () => {
   const { isAuth } = useAuth();
   return (
-    <Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
         <Route path='/*' element={<Frontend />} />
         <Route path='auth/*' element={!isAuth ? <Auth /> : <Navigate to='/dashboard' />} />
         <Route path='dashboard/*' element={<ProtectedRoute Component={Dashboard} />} />
         <Route path='*' element={<Page404 />} />
-    </Routes>
+      </Routes>
+    </>
   )
 }
 
